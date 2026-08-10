fb5efcee38946c5ae1dadd451327288ce425632a
lucas-delazeri <lucasdelazeris@gmail.com>
2026-08-10 14:59:35 -0300

    feat: add toast for user login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -18,28 +19,27 @@ import { LoginService } from '../../services/login.service';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-    loginForm!: FormGroup;
+  loginForm!: FormGroup;
 
-    constructor(
-      private router: Router,
-      private loginService: LoginService
-    ) {
-      this.loginForm = new FormGroup({
-        email: new FormControl('', [Validators.required, Validators.email]),
-        password: new FormControl('', [Validators.required, Validators.minLength(6)])
-      })
-    }
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastService: ToastrService
+  ) {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
 
-    submit() {
-      this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
-        next: () => console.log('success'),
-        error: () => console.log('error')
-      })
-    }
+  submit() {
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => this.toastService.success("Login feito com sucesso!"),
+      error: () => this.toastService.error("Erro inesperado! Tente novamente mais tarde")
+    });
+  }
 
-    navigate() {
-      this.router.navigate(["signup"])
-    }
+  navigate() {
+    this.router.navigate(["signup"]);
+  }
 }
-export { DefaultLoginLayoutComponent };
-
