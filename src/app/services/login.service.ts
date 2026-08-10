f2fa4c8b06c46594f3e10acbefcc8f3688849500
lucas-delazeri <lucasdelazeris@gmail.com>
2026-08-10 15:20:17 -0300

    fix: login with email

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -10,8 +10,8 @@ export class LoginService {
 
   constructor(private httpClient: HttpClient) {  }
 
-    login(name: string, password: string) {
-      return this.httpClient.post<LoginResponse>("/login", { name, password }).pipe(
+    login(email: string, password: string) {
+      return this.httpClient.post<LoginResponse>("/login", { email, password }).pipe(
         tap((value) => {
           sessionStorage.setItem("auth-token", value.token)
           sessionStorage.setItem("username", value.name)
